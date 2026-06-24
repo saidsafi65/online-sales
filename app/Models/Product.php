d4eef4897aa5fd48461dfc0ae9be372e916c5037
Said M Safi <said.safi.056@gmail.com>
2026-06-24 10:53:19 +0300

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'price', 'discount', 'category', 'description', 'image'];
+    protected $fillable = ['name', 'price', 'discount', 'category', 'description', 'image', 'is_out_of_stock'];
 
     /**
      * احسب السعر بعد الخصم
